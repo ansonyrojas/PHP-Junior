27bdf0969d32b645f88bb183c34f90c9583d24dd
Ansony Josue Rojas Ramirez <124784416+ansonyrojas@users.noreply.github.com>
2023-02-22 15:02:25 -0400

    Create connections.php

---
+++ Connection/connections.php
@@ -0,0 +1,70 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "20644880";
+
+
+//Example (MySQLi Object-Oriented)
+
+
+
+//Create connection
+
+$conn = new mysqli($servername,$username,$password); 
+
+
+// Check connection
+
+if ($conn->connect_error){
+
+	die ("Connection Failed: ". $conn->connect_error);
+
+}
+
+echo nl2br( "connected sucessfully\n");
+
+
+///////////////////////////////////////////////
+
+
+// Example (MySQLi Procedural)
+
+
+//Create connection
+
+$conn =  mysqli_connect($servername,$username,$password);
+
+if (!$conn){
+
+	die("Conection Failed: ". mysqli_connect_error());
+}
+
+echo "connected sucessfully";
+
+
+
+
+
+//Example (PDO)
+try{
+
+$conn = new PDO("mysql:host=$servername;", $username, $password);
+
+	//set the PDO error mode to execption 
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	echo "<br>connected sucessfully";	
+
+}catch(PDOExecption $e){
+
+	echo "connected failed: ". $e->getMessage();
+}
+
+
+
+
+
+
+
+?>
