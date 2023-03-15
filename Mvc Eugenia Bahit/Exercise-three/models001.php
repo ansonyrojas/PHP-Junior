5e52df425a060dc6486b11f2b4408daa90f79186
Ansony Josue Rojas Ramirez <124784416+ansonyrojas@users.noreply.github.com>
2023-03-15 06:34:10 -0400

    Create models001.php

---
+++ Mvc Eugenia Bahit/Exercise-three/models001.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+
+
+
+		class Login{
+
+
+					
+
+					function llogin($usuario,$password){
+														
+							$this->usuario = $usuario;
+							$this->password = $password;
+
+					}
+
+					function Texto($tusuario,$tpassword){
+
+							$this->tusuario = $tusuario;
+							$this->tpassword = $tpassword;
+
+
+
+					}
+
+
+
+
+		}
+
+
+	
+
+
+
+
+
+?>
