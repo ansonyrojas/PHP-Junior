1c987d4e8283c39d396db9bbe70d0807b4cf8173
Ansony Josue Rojas Ramirez <124784416+ansonyrojas@users.noreply.github.com>
2023-03-21 15:03:31 -0400

    Add files via upload

---
+++ Mvc Eugenia Bahit/ultimate/usuarios/constants.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+const MODULO = 'usuarios/';
+
+# Controladores
+
+const SET_USER = 'set';
+const GET_USER = 'get';
+const DELETE_USER = 'delete';
+const EDIT_USER = 'edit';
+
+# Vistas
+
+const VIEW_SET_USER = 'agregar';
+const VIEW_GET_USER = 'buscar';
+const VIEW_DELETE_USER = 'borrar';
+const VIEW_EDIT_USER = 'modificar';
+
+
+
+
+
+?>
